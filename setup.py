f0ed13d4b14cc2d8f3e357b1408d4282c988f235
Viggo <victor4711@pm.me>
2026-09-12 23:07:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name="crassus_dcf",
+      version="0.1",
+      packages=["crassus_dcf"],
+      install_requires=["pandas==3.0.5","matplotlib==3.11.2","numpy==2.5.3"])
